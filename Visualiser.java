e36f1410b5c6fccd439ef91da6c9616f699e1485
Abdoulaye <abba@student.codam.nl>
2022-05-25 16:10:49 +0200

    find a small bug on the map

--- Visualiser.java
+++ Visualiser.java
@@ -1,56 +1,64 @@
 import javax.swing.*;
+import javax.swing.JFrame;
 import java.awt.*;
 
-//TODO make it works
-public class Visualiser
-{
-    static int      count;
+public class Visualiser {
+
     static boolean  test;
     static JPanel   panel;
     static JFrame   frame;
+    private int     count;
 
-    public Visualiser() { }
+    GameRules gameRules;
+
+    public Visualiser(GameRules gameRules) {
+        frame = new JFrame("pandemic simulation");
+        frame.setSize(720,720);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(700, 700));
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setVisible(true);
+        this.gameRules = gameRules;
+//        printingOut(gameRules.simulation.getMap());
+//        gameRules.printBoard();
+    }
+
     public void GraphicalInterface(int[][] array, Graphics graphic)
     {
-        int BOX_DIM = 10;
+        int BOX_DIM = 700/ array[0].length;
         for (int i = 0; i < array.length; i++)
         {
-            for (int j = 0; j < array.length; j++)
+            for (int j = 0; j < array[0].length; j++)
             {
                 if(array[i][j] == 0)
                 {
                     graphic.setColor(Color.WHITE);
                 }
-                if(array[i][j] == 1)
+                else if(array[i][j] == 1)
                 {
                     graphic.setColor(Color.BLACK);
                 }
-                if(array[i][j] == 2)
+                else if(array[i][j] == 2)
                 {
                     graphic.setColor(Color.RED);
                 }
-                graphic.fillRect(i * BOX_DIM, j * BOX_DIM, BOX_DIM, BOX_DIM);
+                graphic.fillRect(j  * BOX_DIM, i * BOX_DIM, BOX_DIM -5, BOX_DIM -5);
             }
         }
     }
 
-    public void printingOut(int[][] array, gameRules game)
+    public void printingOut(int[][] array)
     {
         test = true;
+        count = 0;
 
-        frame = new JFrame("pandemic simulation");
-        frame.setSize(800,800);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        panel = new JPanel();
-        panel.setPreferredSize(new Dimension(710, 710));
-        frame.add(panel, BorderLayout.WEST);
-        frame.setVisible(true);
 
-        while(Visualiser.test) {
+
             Graphics graphic = panel.getGraphics();
             GraphicalInterface(array, graphic);
-            game.startSimulation();
-        }
+//            gameRules.startSimulation();
     }
 }
